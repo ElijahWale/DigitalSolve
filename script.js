1181025b7b1fdd07af886b355108acd3e3a2a89b
elijahwale <walexy730@gmail.com>
2021-04-21 14:26:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('nav');
+
+window.onscroll = function(){
+    if(window.pageYOffset > 0){
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+}
+console.log('working');
